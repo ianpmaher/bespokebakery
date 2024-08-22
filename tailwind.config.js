0c96e251ab9c42bcfcc16e7e9a6bb74826a9ec70
Ian Maher <ianpmaher@gmail.com>
2024-08-22 17:07:11 -0400

    new colors who dis

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,15 @@ const config = {
                 docks: "url('https://images.unsplash.com/photo-1561723236-753ad66c3561?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
             },
             colors: {
+                bake : {
+                    "warm-beige": "#F5F0E1",
+                    "cinnamon-brown": "#A67B5B",
+                    "burnt-orange": "#FFFFFF",
+                    "dusty-rose": "#d3a59b",
+                    "espresso": "#4b3a2a",  
+                    "espresso-dark": "#3C2F2F",
+                    "honey-gold": "#F2C057",
+                },
                 custom8008: {
                     "bg-color": "#333a45",
                     "main-color": "#f44c7f",
